b0da71eb876c6ed5aa1bf54e266fdcd435b7df70
Baybhin Gurung <baybhin+github@gmail.com>
2025-11-30 12:52:43 -0500

    Add asyncontextmanager for database startup and close

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,27 @@
 from fastapi import FastAPI
-from controllers.qa import router as true_router 
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from models.database import connect_mongo, close_mongo
 
-app = FastAPI()
-app.include_router(true_router)
+from controllers.qa import router as qa_router
+from controllers.user import router as user_router 
+
+@asynccontextmanager
+async def lifespan(app):
+    await connect_mongo()
+    yield 
+    await close_mongo()
+
+app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"]
+)
+
+app.include_router(qa_router)
+app.include_router(user_router)
 
